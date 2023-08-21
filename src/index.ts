5980570d509bc4cc0ffb50bca767a11b64389926
Max Alva <alva85@gmail.com>
2023-08-21 05:41:32 -0500

    add cors

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,14 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import express from 'express';
+import cors from 'cors';
 import { dbConnection } from './config/db';
 import appRoutes from './routes';
 
 const app = express();
 
 app.use(express.json());
+app.use( cors() );
 
 app.get('/', (_, res) => {
     res.send('Hello world');
